3f4015b20bc9252186cd287d992eb184a706d04f
Nico Huerta <na.huertaf@gmail.com>
2022-09-08 02:04:31 -0300

    super query ready

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -4,11 +4,11 @@ import { Model } from 'mongoose';
 import { Mutex, MutexInterface, Semaphore, SemaphoreInterface, withTimeout } from 'async-mutex';
 
 
-import { Report } from './entities/report.entity';
 import { CreateReportInput } from './dto/create-report.input';
 import { UpdateReportInput } from './dto/update-report.input';
 import { IntegrationService } from '@src/integration/integration.service';
 import { Contaminante, Nivel1, Nivel2, Nivel3, Nivel4 } from '@src/niveles/schema';
+import { Report } from './schema/report.schema';
 
 @Injectable()
 export class ReportService {
@@ -26,14 +26,18 @@ export class ReportService {
     @InjectModel(Contaminante.name)
     private contaminanteModel: Model<Contaminante>,
 
+    @InjectModel(Report.name)
+    private reportModel: Model<Report>,
+
+
     private mutex: Mutex,
     private semaphore: Semaphore,
 
-  ) { 
+  ) {
     this.mutex = new Mutex();
   }
 
-  async generateReport(): Promise<Report> {
+  async generateReport(): Promise<any> {
     const startDate = new Date();
 
     const { ok, data, msg } = await this.integrationService.parseExcel();
@@ -44,103 +48,72 @@ export class ReportService {
 
     //TODO: DONT RETURN ANYTHING UNTIL THE PROCESS IS COMPLETE
 
-    const result = data.map(async (entry, index) => {
+    data.forEach(async (entry, index) => {
 
       await this.mutex.runExclusive(async () => {
 
         //TODO: INSERT REPORT IN REPORT COLLECTION
         //TODO: INSERT REPORTITEM IN REPORTITEM COLLECTION
+        if (index > 0) return;
 
-        console.log(`Starting for index ${index+2}`);
-        //CHECK LEVEL1 IN BD
-        console.log(`findNivel1 for index ${index+2} and nivel1 ${entry.Alcance}`);
-        const { _id: nivel1ID = undefined } = await this.nivel1Model.findOne({ name: String(entry.Alcance).trim() });
-        console.log(`-- ${nivel1ID}`);
+        console.log(`Starting for index ${index + 2}`);
+        // //CHECK LEVEL1 IN BD
+        // console.log(`findNivel1 for index ${index+2} and nivel1 ${entry.Alcance}`);
+        // const { _id: nivel1ID = undefined } = await this.nivel1Model.findOne({ name: String(entry.Alcance).trim() });
+        // console.log(`-- ${nivel1ID}`);
 
-        //IF LEVEL2 NOT EXIST, OMMIT THIS LINE (ERROR, CANT CALCULATE)
-        if(!entry['Nivel 2']) return;
+        // //IF LEVEL2 NOT EXIST, OMMIT THIS LINE (ERROR, CANT CALCULATE)
+        // if(!entry['Nivel 2']) return;
 
-        //CHECK LEVEL2 IN BD
-        console.log(`findNivel2 for index ${index+2} and nivel2 ${entry['Nivel 2']}`);
-        const { _id: nivel2ID = undefined } = await this.nivel2Model.findOne({ name: String(entry['Nivel 2']).trim() });
-        console.log(`-- ${nivel2ID}`);
-        
+        // //CHECK LEVEL2 IN BD
+        // console.log(`findNivel2 for index ${index+2} and nivel2 ${entry['Nivel 2']}`);
+        // const { _id: nivel2ID = undefined } = await this.nivel2Model.findOne({ name: String(entry['Nivel 2']).trim() });
+        // console.log(`-- ${nivel2ID}`);
+
         //TODO: CHECK LEVEL3 IN BD WITH DATA OF LEVEL2
         //TODO: CHECK LEVEL4 IN BD WITH DATA OF LEVEL2 & LEVEL3
         //TODO: CHECK CONTAMINANTE IN BD WITH DATA OF LEVEL2 & LEVEL3 & LEVEL4
 
         //TODO: CALCULATE THE DATA AND INSERT IN REPORTRESULT COLLECTION
-        
-        console.log(`Finished for index ${index+2}`);
+
+
+        console.log(entry['Nivel 2']);
+        const resp = await this.contaminanteModel
+          .aggregate()
+          .lookup({
+            from: 'nivel2',
+            localField: 'nivel2',
+            foreignField: '_id',
+            as: 'nivel2',
+          })
+          .match({
+            'nivel2.name': entry['Nivel 2'],
+          })
+          .lookup({
+            from: 'nivel3',
+            localField: 'nivel3',
+            foreignField: '_id',
+            as: 'nivel3',
+          })
+          .match({
+            'nivel3.name': entry['Nivel 3'],
+          })
+          .lookup({
+            from: 'nivel4',
+            localField: 'nivel4',
+            foreignField: '_id',
+            as: 'nivel4',
+          })
+          .match({
+            'nivel4.name': entry['Nivel 4'],
+          })
+
+        console.log(`Tamaño resp: ${resp.length}`);
+        console.log("resp: ", resp);
+        console.log(`Finished for index ${index + 2}`);
       });
 
     });
-
-
-    //OLD CODE -- DO NOT USE --- ONLY REFERAL FOR THE NEW CODE
-
-      // const calculatedResult = data.map(async (entry, index) => {
-
-      //   try {
-      //     console.log(`evaluating index: ${index}`);
-      //     console.log(await this.nivel1Model.findOne({ name: entry.Alcance }))
-      //     // const { _id: nivel1ID } = await this.nivel1Model.findOne({ name: entry.Alcance });
-      //     // console.log(nivel1ID);
-      //     // const { _id: nivel2ID } = await this.nivel2Model.findOne({
-      //     //   where: {
-      //     //     $and: [
-      //     //       { name: entry['Nivel 2'] },
-      //     //       { nivel1: {$eq: nivel1ID} },
-      //     //     ],
-      //     //   }
-      //     // });
-      //     // console.log(nivel2ID);
-      //     // const { _id: nivel3ID } = await this.nivel3Model.findOne({
-      //     //   where: {
-      //     //     $and: [
-      //     //       { name: entry['Nivel 3'] },
-      //     //       { nivel2: nivel2ID },
-      //     //     ],
-      //     //   }
-      //     // });
-
-      //     // const { _id: nivel4ID } = await this.nivel4Model.findOne({
-      //     //   where: {
-      //     //     $and: [
-      //     //       { name: entry['Nivel 4'] },
-      //     //       { nivel3: nivel3ID },
-      //     //       { nivel2: nivel2ID },
-      //     //     ],
-      //     //   }
-      //     // });
-
-      //     // const contaminantes = await this.contaminanteModel.find({
-      //     //   where: {
-      //     //     $and: [
-      //     //       { nivel4: nivel4ID },
-      //     //       { nivel3: nivel3ID },
-      //     //       { nivel2: nivel2ID },
-      //     //     ],
-      //     //   }
-      //     // });
-
-      //     // console.log(
-      //       // nivel1ID,
-      //       // nivel2ID,
-      //       // nivel3ID,
-      //       // nivel4ID,
-      //       // { contaminantesLength: contaminantes.length, ...contaminantes },
-      //     // )
-
-      //     return;
-
-      //   } catch (err) {
-      //     //Handle this error somehow in the future, this is when the search fail
-      //     return;
-      //   }
-      // });
-
-
 
     return {
       ok: true,
