55ad37ea0c9db522287fb30ce63c1f188b91605c
Carlos Salazar Silva <73184352+Carlangas11@users.noreply.github.com>
2022-09-09 02:11:23 -0400

    feat getReportItems

--- src/niveles/niveles.resolver.ts
+++ src/niveles/niveles.resolver.ts
@@ -14,7 +14,7 @@ export class NivelesResolver {
   constructor(private readonly nivelesService: NivelesService) {}
 
   @Query(() => String, { name: 'seed', nullable: true })
-  @UseGuards(JwtAuthGuard)
+  //   @UseGuards(JwtAuthGuard)
   async seed(@Args('lvl') lvl: string) {
     return await this.nivelesService.seed(lvl)
   }
@@ -33,63 +33,62 @@ export class NivelesResolver {
     return await this.nivelesService.findOne(id)
   }
 
-    // @Mutation(() => nivel1Model, { name: 'createNivel1', nullable: true })
-    // @UseGuards(JwtAuthGuard)
-    // async createNivel1(@Args('nivel1Input') nivel1Input: Nivel1Input ): Promise<typeof nivel1Model> {
-    //     return await this.nivelesService.createLevel1(nivel1Input)
-    // }
+  // @Mutation(() => nivel1Model, { name: 'createNivel1', nullable: true })
+  // @UseGuards(JwtAuthGuard)
+  // async createNivel1(@Args('nivel1Input') nivel1Input: Nivel1Input ): Promise<typeof nivel1Model> {
+  //     return await this.nivelesService.createLevel1(nivel1Input)
+  // }
 
+  // @Mutation(() => nivel1Model, { name: 'updateNivel1', nullable: true })
+  // @UseGuards(JwtAuthGuard)
+  // async updateNivel1(@Args('updateNivel1Input', ParseMongObjectIdPipe) updateNivel1Input: UpdateNivel1Input ): Promise<typeof nivel1Model> {
+  //     return await this.nivelesService.updateLevel1(updateNivel1Input)
+  // }
 
-    // @Mutation(() => nivel1Model, { name: 'updateNivel1', nullable: true })
-    // @UseGuards(JwtAuthGuard)
-    // async updateNivel1(@Args('updateNivel1Input', ParseMongObjectIdPipe) updateNivel1Input: UpdateNivel1Input ): Promise<typeof nivel1Model> {
-    //     return await this.nivelesService.updateLevel1(updateNivel1Input)
-    // }
+  // @Mutation(() => nivel1Model, { name: 'deleteNivel1', nullable: true })
+  // @UseGuards(JwtAuthGuard)
+  // async deleteNivel1(@Args('id', ParseMongoIdPipe) id: string ): Promise<any> {
+  //     return await this.nivelesService.deleteLevel1(id)
+  // }
 
+  @Query(() => contaminanteResponse)
+  async getContaminantes(
+    @Args('pagination') pagination?: number,
+  ): Promise<contaminanteResponse> {
+    return this.nivelesService.getContaminantes(pagination)
+  }
 
-    // @Mutation(() => nivel1Model, { name: 'deleteNivel1', nullable: true })
-    // @UseGuards(JwtAuthGuard)
-    // async deleteNivel1(@Args('id', ParseMongoIdPipe) id: string ): Promise<any> {
-    //     return await this.nivelesService.deleteLevel1(id)
-    // }
+  @Mutation(() => contaminanteModel, {
+    name: 'createContaminante',
+    nullable: true,
+  })
+  @UseGuards(JwtAuthGuard)
+  async createContaminante(
+    @Args('contaminanteInput') contaminanteInput: ContaminanteInput,
+  ): Promise<typeof contaminanteModel> {
+    return await this.nivelesService.createContaminante(contaminanteInput)
+  }
 
-    @Query(() => contaminanteResponse)
-    async getContaminantes(
-        @Args('pagination') pagination?: number,
-    ): Promise<contaminanteResponse> {
-        return this.nivelesService.getContaminantes(pagination)
-    }
+  @Mutation(() => contaminanteModel, {
+    name: 'updateContaminante',
+    nullable: true,
+  })
+  @UseGuards(JwtAuthGuard)
+  async updateContaminante(
+    @Args('updateContaminanteInput', ParseMongObjectIdPipe)
+    updateContaminanteInput: UpdateContaminanteInput,
+  ): Promise<typeof contaminanteModel> {
+    return await this.nivelesService.updateContaminante(updateContaminanteInput)
+  }
 
-    @Mutation(() => contaminanteModel, { 
-        name: 'createContaminante', 
-        nullable: true 
-    })
-    @UseGuards(JwtAuthGuard)
-    async createContaminante(
-        @Args('contaminanteInput') contaminanteInput: ContaminanteInput 
-    ): Promise<typeof contaminanteModel> {
-        return await this.nivelesService.createContaminante(contaminanteInput)
-    }
-
-    @Mutation(() => contaminanteModel, { 
-        name: 'updateContaminante', 
-        nullable: true 
-    })
-    @UseGuards(JwtAuthGuard)
-    async updateContaminante(
-        @Args('updateContaminanteInput', ParseMongObjectIdPipe) updateContaminanteInput: UpdateContaminanteInput
-    ): Promise<typeof contaminanteModel> {
-        return await this.nivelesService.updateContaminante(updateContaminanteInput)
-    }
-
-    @Mutation(() => contaminanteModel, { 
-        name: 'deleteContaminante', 
-        nullable: true 
-    })
-    @UseGuards(JwtAuthGuard)
-    async deleteContaminante(
-        @Args('id', ParseMongoIdPipe) id: string 
-    ): Promise<typeof contaminanteModel> {
-        return await this.nivelesService.deleteContaminante(id)
-    }
+  @Mutation(() => contaminanteModel, {
+    name: 'deleteContaminante',
+    nullable: true,
+  })
+  @UseGuards(JwtAuthGuard)
+  async deleteContaminante(
+    @Args('id', ParseMongoIdPipe) id: string,
+  ): Promise<typeof contaminanteModel> {
+    return await this.nivelesService.deleteContaminante(id)
+  }
 }
