e662857e6ab12f9d582e6f7faea5b078acd8dc64
Nico Huerta <nhuertaf@icloud.com>
2022-10-04 00:20:35 -0300

    graphs ready to use

--- src/graphs/entities/graphs.entity.ts
+++ src/graphs/entities/graphs.entity.ts
@@ -1,26 +1,100 @@
-import { Field, ObjectType } from "@nestjs/graphql";
-import { IsArray, isIn, IsIn, IsMongoId, IsOptional } from "class-validator";
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { IsArray, IsMongoId, IsOptional, IsString, IsIn, MaxLength, ArrayNotEmpty, ArrayMaxSize, ArrayUnique } from 'class-validator';
 
-@ObjectType()
+
+//////////////////////// request graficos below ////////////////////////
+@InputType()
 export class GenerateGraphsRequest {
 
     @Field()
     @IsMongoId({ message: 'El id del reporte no es válido' })
     idReport: string;
 
-    @Field()
+    @Field(() => [Number], { nullable: true })
     @IsOptional()
-    @IsArray()
-    @IsIn(
-        [[1, 2], [2, 3], [1, 3], [1], [2], [3], [2, 1], [3, 2], [3, 1]],
-        { message: 'El tipo de alcances solicitados no es valido. Type: array[number] | numbers: 1,2,3 | max_length: 2 | for all data, dont add alcances property' }
-    )
-    alcances?: number[];
+    @IsArray({ message: 'alcances no es valido. Type: array[number]' })
+    @IsIn([1, 2, 3], {
+        message: 'El alcance solicitado no es valido. numbers: 1,2,3 ',
+        each: true,
+    })
+    @ArrayNotEmpty()
+    @ArrayMaxSize(2, {
+        message: 'El numero de alcances solicitados no es valido.  max_length: 2',
+    })
+    @ArrayUnique()
 
+    alcances?: number[];
+}
+//////////////////////// request graficos adobe ////////////////////////
+
+//////////////////////// response graficos below ////////////////////////
+@ObjectType()
+export class TotalEmissionsDTO {
+
+    @Field()
+    @IsString()
+    emisionTotal: number;
+
+    @Field()
+    @IsString()
+    unidadMedida: string;
+}
+
+@ObjectType()
+export class EmisionesPorClasificacionDTO {
+
+    @Field()
+    @IsString()
+    nombre: string;
+
+    @Field()
+    @IsString()
+    valor: number;
+
+    @Field()
+    @IsString()
+    unidadMedida: string;
+}
+
+@ObjectType()
+export class AlcanceDTO {
+
+    @Field()
+    totalPorAlcance: TotalEmissionsDTO;
+
+    @Field(() => [EmisionesPorClasificacionDTO])
+    emisionesPorUnidad: EmisionesPorClasificacionDTO[];
+
+    @Field(() => [EmisionesPorClasificacionDTO])
+    emisionesPorNivel2: EmisionesPorClasificacionDTO[];
+
+    @Field(() => [EmisionesPorClasificacionDTO])
+    emisionesPorNivel3: EmisionesPorClasificacionDTO[];
+}
+
+@ObjectType()
+export class EmisionesPorAlcanceDTO {
+    @Field(() => AlcanceDTO, { nullable: true })
+    @IsOptional()
+    alcance1?: AlcanceDTO;
+
+    @Field(() => AlcanceDTO, { nullable: true })
+    @IsOptional()
+    alcance2?: AlcanceDTO;
+
+    @Field(() => AlcanceDTO, { nullable: true })
+    @IsOptional()
+    alcance3?: AlcanceDTO;
 }
 
 @ObjectType()
 export class GenerateGraphsResponse {
 
+    @Field(() => TotalEmissionsDTO)
+    totalEmissions: TotalEmissionsDTO;
 
+    @Field(() => EmisionesPorAlcanceDTO)
+    totalEmissionsByAlcance: EmisionesPorAlcanceDTO;
 }
+
+//////////////////////// response graficos adobe ////////////////////////
--- src/graphs/graphs.resolver.ts
+++ src/graphs/graphs.resolver.ts
@@ -1,13 +1,13 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { GenerateGraphsRequest, GenerateGraphsResponse } from './entities/graphs.entity';
 import { GraphsService } from './graphs.service';
 
 @Resolver()
 export class GraphsResolver {
-  constructor(private readonly graphsService: GraphsService) {}
+  constructor(private readonly graphsService: GraphsService) { }
 
   @Query(() => GenerateGraphsResponse)
-  async getGraphs( @Args({ name: 'input' }) input: GenerateGraphsRequest ) {
+  async getGraphs(@Args({ name: 'input' }) input: GenerateGraphsRequest) {
     return await this.graphsService.getGraphs(input);
   }
 
